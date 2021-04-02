1a6763b74c89efa50916b17188d82785f3b2308e
Aguirre Marcelo <exadalx@gmail.com>
2021-04-01 22:56:38 -0300

    V2.  Corrijo errores del ejercicio

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -2,28 +2,30 @@
 
 int cuadrado( int numero); // punto a
 
-void tipovoid( int numero); // punto b
+void tipovoid( int *numero); // punto b
 
-void muestre(int numero); //punto c
+void muestre(int *numero); //punto c
 
-void invertir(int ParametroA , int parametroB); //punto d - f
+void invertir(int *ParametroA , int *parametroB); //punto d - f
 
-void orden(int ParametroA , int parametroB); //punto e - f
+void orden(int *Parametroa , int *parametrob); //punto e - f
 
 int main() {
-    int variable, parametroA , parametroB ;
+    int variable, parametroA , parametroB, parametroa , parametrob ;
     int numero;
     printf("Hello, world!\n");
 
     printf("Ingrese un numero: ");
     scanf("%d", &variable);
+    printf("Numero ingresado: %d\n", variable);
+    printf("---------------------\n");
     numero = cuadrado(variable);
-
-
     printf("El cuadrado del numero: %d\n", numero);
-    tipovoid(variable);
-
-    muestre(variable);
+    printf("---------------------\n");
+    tipovoid(&variable);
+    printf("El cuadrado del numero-(void): %d\n", variable);
+    printf("---------------------\n");
+    muestre(&variable);
     printf("---------------------\n");
 
     printf("ingrese 2 valores\nPrimer valor: ");
@@ -32,9 +34,18 @@ int main() {
     scanf("%d", &parametroB);
     printf("---------------------\n");
 
-    invertir(parametroA , parametroB);
+    invertir(&parametroA , &parametroB);
+    printf("valor invertido del parametroA: %d\n", parametroA);
+    printf("valor invertido del parametroB: %d\n", parametroB);
     printf("---------------------\n");
-    orden( parametroA ,  parametroB);
+    printf("ingrese 2 valores\nPrimer valor: ");
+    scanf("%d", &parametroa);
+    printf("Sengundo valor: ");
+    scanf("%d", &parametrob);
+    printf("---------------------\n");
+    orden( &parametroa ,  &parametrob);
+    printf("orden1 (valor mas chicos): %d\n", parametroa);
+    printf("orden2 (valor mas grande): %d\n", parametrob);
    
     return 0;
 }
@@ -45,46 +56,37 @@ int cuadrado( int numero){
 
 }
 
-void tipovoid (int numero){
+void tipovoid (int *numero){
 
-    int aux ;
-
-    aux = numero * numero ;
-    printf("El cuadrado del numero(void): %d\n", aux);
     
-}
 
-void muestre(int numero){
-
-    printf("Direccion de la variable: %u\n", &numero);
-    printf("Contenido de la variable: %d\n", numero);
-}
-
-void invertir(int parametroA , int parametroB){
-
-    int aux;
-
-    aux = parametroA;
-    parametroA = parametroB;
-    parametroB = aux;
-    
-    printf("valor invertido del parametroA: %d\n", parametroA);
-    printf("valor invertido del parametroB: %d\n", parametroB);
-
-}
-
-void orden(int parametroA , int parametroB){
-
-    int aux;
-
-    if (parametroA > parametroB)
-    {
-        aux = parametroA;
-        parametroA = parametroB;
-        parametroB = aux;
-    }
+     *numero*= *numero ;
       
-    printf("orden1 (valor mas chicos): %d\n", parametroA);
-    printf("orden2 (valor mas grande): %d\n", parametroB);
+}
+
+void muestre(int *numero){
+
+    printf("Direccion de la variable: %u\n", numero);
+    printf("Contenido de la variable: %d\n", *numero);
+}
+
+void invertir(int *parametroA , int *parametroB){
+
+    int aux;
+
+    aux = *parametroA;
+    *parametroA = *parametroB;
+    *parametroB = aux;
+    
+}
+
+void orden(int *parametroa , int *parametrob){
+
+
+    if (*parametroa > *parametrob)
+    {
+        invertir( parametroa , parametrob);
+    }
+    
 
 }
